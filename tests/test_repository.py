52e3558e8f43ef6d79d2077205165bef2a0353a7
JohnX451 <50927491+JohnX451@users.noreply.github.com>
2023-11-19 11:17:56 +0100

    updated existing unit tests
    updated existing unit tests for
    - repository
    - entity

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,33 +1,37 @@
 import pytest
 
-from src.entity.post import PostDTO
 from src.entity.user import UserDTO
+from src.entity.post import PostDTO
 from src.repository.dbdict import DBAccess
 
 
 def test_insert_get_post():
-    test_id: int = 5
+    test_id: int = 0
+    post_dto = PostDTO(post_id=test_id, user_id=1, text="post0")
     dba_default = DBAccess()
-    dba_default.insert_post(PostDTO(test_id))
+    dba_default.insert_post(post_dto)
     result = dba_default.get_post(test_id)
 
-    assert result.user_id == test_id
+    assert result.post_id == test_id
+    assert result.text == "post0"
 
 
 def test_insert_get_user():
-    test_id: int = 5
+    test_id: int = 0
+    user_dto = UserDTO(user_id=test_id, Username="user0")
     dba_default = DBAccess()
-    dba_default.insert_user(UserDTO(test_id))
+    dba_default.insert_user(user_dto)
     result = dba_default.get_user(test_id)
 
     assert result.user_id == test_id
+    assert result.Username == "user0"
 
 
 def test_insert_get_all_posts():
     dba_default = DBAccess()
-    dba_default.insert_post(PostDTO(0))
-    dba_default.insert_post(PostDTO(1))
-    dba_default.insert_post(PostDTO(2))
+    dba_default.insert_post(PostDTO(post_id=1, user_id=1, text="post1"))
+    dba_default.insert_post(PostDTO(post_id=2, user_id=1, text="post2"))
+    dba_default.insert_post(PostDTO(post_id=3, user_id=1, text="post3"))
     result = dba_default.get_all_posts()
 
     # 4 because the database wasn't cleared before adding the new elements
@@ -36,11 +40,10 @@ def test_insert_get_all_posts():
 
 def test_insert_get_all_users():
     dba_default = DBAccess()
-    dba_default.insert_user(UserDTO(0))
-    dba_default.insert_user(UserDTO(1))
-    dba_default.insert_user(UserDTO(2))
+    dba_default.insert_user(UserDTO(user_id=1, Username="user1"))
+    dba_default.insert_user(UserDTO(user_id=2, Username="user2"))
+    dba_default.insert_user(UserDTO(user_id=3, Username="user3"))
     result = dba_default.get_all_users()
-    print(result)
 
     # 4 because the database wasn't cleared before adding the new elements
     assert len(result) == 4
