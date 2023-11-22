562f4fed33958e35f8780d3295e288c95e70ea89
AK <50927491+JohnX451@users.noreply.github.com>
2023-11-22 15:01:44 +0100

    Update main.py
    removed database call in main

--- src/main.py
+++ src/main.py
@@ -4,10 +4,7 @@ from repository.db_access import DBAccess
 
 
 def main():
-    ctx = DBAccess()
-    ctx.insert_post(PostDTO(post_id=1, user_id=1))
-    ctx.insert_user(UserDTO(user_id=1))
-    print(PostDTO.list_as_json(ctx.get_all_posts()))
+    print("main.py started...")
 
 
 if __name__ == '__main__':
