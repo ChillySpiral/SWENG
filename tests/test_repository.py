fe9062df803a273d8c66027b3257daf3d0f2a0f7
AK <50927491+JohnX451@users.noreply.github.com>
2023-11-22 15:01:31 +0100

    updated unit tests

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -5,44 +5,44 @@ from src.entity.post import PostDTO
 from src.repository.db_access import DBAccess
 
 
-def test_insert_get_post():
+def test_create_get_post():
     test_id: int = 0
     post_dto = PostDTO(post_id=test_id, user_id=1, text="post0")
     dba_default = DBAccess()
-    dba_default.insert_post(post_dto)
+    dba_default.create_post(post_dto)
     result = dba_default.get_post(test_id)
 
     assert result.post_id == test_id
     assert result.text == "post0"
 
 
-def test_insert_get_user():
+def test_create_get_user():
     test_id: int = 0
-    user_dto = UserDTO(user_id=test_id, Username="user0")
+    user_dto = UserDTO(user_id=test_id, username="user0")
     dba_default = DBAccess()
-    dba_default.insert_user(user_dto)
+    dba_default.create_user(user_dto)
     result = dba_default.get_user(test_id)
 
     assert result.user_id == test_id
-    assert result.Username == "user0"
+    assert result.username == "user0"
 
 
-def test_insert_get_all_posts():
+def test_create_get_all_posts():
     dba_default = DBAccess()
-    dba_default.insert_post(PostDTO(post_id=1, user_id=1, text="post1"))
-    dba_default.insert_post(PostDTO(post_id=2, user_id=1, text="post2"))
-    dba_default.insert_post(PostDTO(post_id=3, user_id=1, text="post3"))
+    dba_default.create_post(PostDTO(post_id=1, user_id=1, text="post1"))
+    dba_default.create_post(PostDTO(post_id=2, user_id=1, text="post2"))
+    dba_default.create_post(PostDTO(post_id=3, user_id=1, text="post3"))
     result = dba_default.get_all_posts()
 
     # 4 because the database wasn't cleared before adding the new elements
     assert len(result) == 4
 
 
-def test_insert_get_all_users():
+def test_create_get_all_users():
     dba_default = DBAccess()
-    dba_default.insert_user(UserDTO(user_id=1, Username="user1"))
-    dba_default.insert_user(UserDTO(user_id=2, Username="user2"))
-    dba_default.insert_user(UserDTO(user_id=3, Username="user3"))
+    dba_default.create_user(UserDTO(user_id=1, username="user1"))
+    dba_default.create_user(UserDTO(user_id=2, username="user2"))
+    dba_default.create_user(UserDTO(user_id=3, username="user3"))
     result = dba_default.get_all_users()
 
     # 4 because the database wasn't cleared before adding the new elements
--- tests/test_rest_api.py
+++ tests/test_rest_api.py
@@ -1,4 +1,5 @@
 import pytest
+import json
 from fastapi.testclient import TestClient
 from src.endpoints.rest_api import app
 from src.repository.db_access import DBAccess
@@ -8,15 +9,43 @@ from src.entity.post import PostDTO
 client = TestClient(app)
 dba_default = DBAccess()
 
-# info: requires installation of httpx
-
 
 def pytest_sessionstart(session):
-    dba_default.insert_post(PostDTO(post_id=1, user_id=1, text="post1"))
-    dba_default.insert_user(UserDTO(user_id=1, Usernane="user1"))
+    dba_default.create_post(PostDTO(post_id=1, user_id=1, text="post1"))
+    dba_default.create_user(UserDTO(user_id=1, Usernane="user1"))
 
 
 def test_get_post():
-    response = client.get("/read/posts/1")
+    response = client.get("/posts/1")
     assert response.status_code == 200
     assert response.json() == {"post_id": 1, "user_id": 1, "text": "post1", "image": ""}
+
+
+def test_get_user():
+    response = client.get("/users/1")
+    assert response.status_code == 200
+    assert response.json() == {"user_id": 1, "password": "", "username": "user1"}
+
+
+def test_create_post():
+    response = client.post(
+        "/posts/create/",
+        json={"post_id": 2, "user_id": 1, "text": "test create post", "image": ""},
+    )
+    assert response.status_code == 200
+
+
+def test_create_user():
+    response = client.post(
+        "/users/create/",
+        json={"user_id": 2, "username": "user2", "password": "asdf"},
+    )
+    assert response.status_code == 200
+
+
+def test_get_all_posts():
+    response = client.get("/posts/")
+    assert response.status_code == 200
+    assert len(response.json()) == 4
+
+
