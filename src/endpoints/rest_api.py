0a49a1f5de4a18d548c07322200b4a6fd7640736
AK <50927491+JohnX451@users.noreply.github.com>
2023-11-22 15:01:23 +0100

    renamed and extended REST API methods
    naming convention pass, extension

--- src/endpoints/rest_api.py
+++ src/endpoints/rest_api.py
@@ -1,9 +1,8 @@
-from typing import Dict, Any
-
 from fastapi import FastAPI
 from fastapi.openapi.utils import get_openapi
 
 from src.entity.post import PostDTO
+from src.entity.user import UserDTO
 from src.repository.db_access import DBAccess
 
 app = FastAPI()
@@ -15,19 +14,34 @@ class RestAPI:
         app.openapi = RestAPI.generate_openapi_contract()
 
     @staticmethod
-    @app.post("/create/posts/", tags=["Create", "Posts"])
-    async def read_post(post: PostDTO):
-        db_access.insert_post(post)
+    @app.post("/posts/create/", tags=["Create", "Posts"])
+    async def create_post(post: PostDTO):
+        db_access.create_post(post)
 
     @staticmethod
-    @app.get("/read/posts/{post_id}", tags=["Read", "Posts"])
-    async def read_post(post_id: int) -> PostDTO:
+    @app.get("/posts/{post_id}", tags=["Get", "Posts"])
+    async def get_post(post_id: int) -> PostDTO:
         return db_access.get_post(post_id)
 
     @staticmethod
-    @app.get("/read/posts/", tags=["Read", "Posts"])
-    async def read_posts() -> list[PostDTO]:
+    @app.get("/posts/{user_id}", tags=["Get", "Posts"])
+    async def get_post_by_user(user_id: int) -> list[PostDTO]:
+        return db_access.get_posts_by_user(user_id)
+
+    @staticmethod
+    @app.get("/posts/", tags=["Get", "Posts"])
+    async def get_all_posts() -> list[PostDTO]:
         return db_access.get_all_posts()
+
+    @staticmethod
+    @app.post("/users/create/", tags=["Create", "Users"])
+    async def create_user(user: UserDTO):
+        db_access.create_user(user)
+
+    @staticmethod
+    @app.get("/users/{user_id}", tags=["Get", "Users"])
+    async def get_post(user_id: int) -> UserDTO:
+        return db_access.get_user(user_id)
 
     @staticmethod
     def generate_openapi_contract():
