498940b88b867e2b3d6041132cae3cabd3c67844
JohnX451 <50927491+JohnX451@users.noreply.github.com>
2023-11-24 15:33:23 +0100

    Update test_repository.py
    - to reflect changes in user entities

--- tests/test_repository.py
+++ tests/test_repository.py
@@ -3,12 +3,15 @@ from src.entity.user_dto import UserDTO
 from src.entity.post_dto import PostDTO
 from src.repository.crud import CRUD
 from src.model.post_model import PostResponse, PostModel, PostCreateModel
-from src.model.user_model import UserModel, UserResponse, UserLoginResponse, UserUpdateModel
+from src.model.user_model import UserModel, UserResponse, UserLoginResponse, UserUpdateModel, UserBioModel
 
 
 def test_create_user():
     username_test: str = "Test"
     password_test: str = "PwTest"
-    result: UserResponse = CRUD().insert_user(UserModel(username=username_test, password=password_test))
+    bio_test: str = "I am some user"
+    result: UserResponse = CRUD().insert_user(UserBioModel(
+        username=username_test, password=password_test, bio=bio_test))
 
     assert result.username == username_test
+    assert result.bio == bio_test
