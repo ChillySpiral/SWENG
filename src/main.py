c85d84415dd90f0522fe2770650c74c649204db0
Danny <150384557+Ai23m017@users.noreply.github.com>
2023-12-06 19:16:42 +0100

    Revert "Feature/database initial"

--- src/main.py
+++ src/main.py
@@ -1,25 +1,10 @@
-import os
-
-from dotenv import load_dotenv
-from sqlalchemy import URL
-from sqlalchemy import create_engine
-from sqlalchemy.schema import CreateSchema
-from entity.entities import Base
-from src.repository.db_repository import Repository
+from src.entity.post_dto import PostDTO
+from src.entity.user_dto import UserDTO
+from repository.crud import CRUD
 
 
 def main():
-    repository = Repository()
-    user_dto = repository.insert_user(username="test", password="test", bio="hello there world")
-    user_dto = repository.get_user(user_dto.user_id)
-    user_dto = repository.update_user(user_id= user_dto.user_id, username="test", password="test", bio="hello there "
-                                                                                                       "updated world")
-    user_dto = repository.get_user(user_dto.user_id)
-    repository.insert_user(username="user2", password="test", bio="hello there world")
-
-    print(repository.get_all_users())
-    repository.delete_user(user_dto.user_id)
-    repository.delete_user(user_dto.user_id)
+    print("main.py started...")
 
 
 if __name__ == '__main__':
