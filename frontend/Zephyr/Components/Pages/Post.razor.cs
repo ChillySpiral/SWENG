07de8e47c9aa6714192046ad515aaa15718a80fc
ChillySpiral <chrges98@gmail.com>
2023-12-28 12:19:58 +0100

    Added Post Page

--- frontend/Zephyr/Components/Pages/Post.razor.cs
+++ frontend/Zephyr/Components/Pages/Post.razor.cs
@@ -1,6 +1,48 @@
-﻿namespace Zephyr.Components.Pages
+﻿using Microsoft.AspNetCore.Components;
+using Zephyr.Data;
+using Zephyr.Data.ViewModels;
+
+namespace Zephyr.Components.Pages
 {
     public partial class Post
     {
+        [Parameter]
+        public string PostId
+        {
+            get => _postId.ToString();
+            set
+            {
+                if (Guid.TryParse(value, out _postId))
+                {
+
+                }
+            }
+        }
+
+        [Inject]
+        public IBusinessLayer BusinessLayer { get; set; }
+
+        private PostViewModel _postViewModel { get; set; }
+        private Guid _postId;
+
+        public bool IsLoading { get; set; } = true;
+
+        public Post() { }
+
+        protected override async void OnParametersSet()
+        {
+            _postViewModel = await BusinessLayer.GetPost(_postId) ?? new PostViewModel()
+            {
+                Id = _postId,
+            };
+
+            if (_postViewModel?.User.Id != null)
+            {
+                _postViewModel.User = await BusinessLayer.GetUser(_postViewModel.User.Id);
+            }
+
+            IsLoading = false;
+            StateHasChanged();
+        }
     }
 }
